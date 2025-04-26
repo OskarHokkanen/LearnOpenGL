b7afe15c1360dad039f53f2bac4a8137e096d11e
Oskar Hokkanen Eriksson <hokkanen.oskar@gmail.com>
2025-04-26 12:03:02 +0200

    Moves shaders outside of main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -10,18 +13,48 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char *vertexShaderSource = "#version 330 core\n"
-                                 "layout (location = 0) in vec3 aPos;\n"
-                                 "void main()\n"
-                                 "{\n"
-                                 "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-                                 "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
-                                   "out vec4 FragColor;\n"
-                                   "void main()\n"
-                                   "{\n"
-                                   "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-                                   "}\n\0";
+// const char *vertexShaderSource = "#version 330 core\n"
+//                                  "layout (location = 0) in vec3 aPos;\n"
+//                                  "out vec4 vertexColor;\n"
+//                                  "void main()\n"
+//                                  "{\n"
+//                                  "   gl_Position = vec4(aPos, 1.0);\n"
+//                                  "   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
+//                                  "}";
+
+// const char *fragmentShaderSource = "#version 330 core\n"
+//                                    "out vec4 FragColor;\n"
+//                                    "in vec4 vertexColor;\n"
+//                                    "void main()\n"
+//                                    "{\n"
+//                                    "   FragColor = vertexColor;\n"
+//                                    "}\n\0";
+
+// Load shader source code from file
+
+std::string loadShaderSource(const char *filepath)
+{
+    std::ifstream file;
+    std::stringstream buffer;
+
+    file.open(filepath);
+    if (file.is_open())
+    {
+        buffer << file.rdbuf();
+    }
+    else
+    {
+        std::cout << "Failed to open " << filepath << std::endl;
+    }
+    file.close();
+    return buffer.str();
+}
+
+std::string vertexCode = loadShaderSource("src/shaders/vertex.shader");
+std::string fragmentCode = loadShaderSource("src/shaders/fragment.shader");
+
+const char *vertexShaderSource = vertexCode.c_str();
+const char *fragmentShaderSource = fragmentCode.c_str();
 
 int main()
 {
